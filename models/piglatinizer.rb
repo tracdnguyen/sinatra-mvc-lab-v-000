7d79b9a903496ecfb6872ad06b4d4334d3d146e9
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-27 00:00:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,12 +2,13 @@ class PigLatinizer
   attr_accessor :text
 
   def piglatinize(word)
-    if word =~ (/\A[aeiou]/i)
-        word = word + 'ay'
-      elsif word =~ (/\A[^aeiou]/i)
-        match = /\A[^aeiou]/i.match(word)
-        word = match.post_match + match.to_s + 'ay'
-      end
-      word
-  end
+    vowels = %w{a e i o u}
+    sent.gsub(/(\A|\s)\w+/) do |str|
+            str.strip!
+        while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+            str += str[0]
+            str = str[1..-1]
+        end
+        str  = ' ' + str + 'ay'
+    end.strip
 end
